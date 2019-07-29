2643601aff60f555b11ba86bf3465ea381d98b58
EAGLE <eagle2990@protonmail.com>
2019-07-29 13:33:21 -0400

    Searching queries

--- contacts/services.py
+++ contacts/services.py
@@ -2,17 +2,34 @@ import requests
 from .models import Contact
 
 
-def get_remote_contacts():
-    url = 'http://www.mocky.io/v2/581335f71000004204abaf83'
-    req = requests.get(url, headers={'Connection': 'close'})
-    if (req.status_code == 200):
-        return req.json()['contacts']
-    return []
+class ContactsService:
 
+    def get_remote_contacts(self):
+        url = 'http://www.mocky.io/v2/581335f71000004204abaf83'
+        req = requests.get(url, headers={'Connection': 'close'})
+        if (req.status_code == 200):
+            return req.json()['contacts']
+        return []
 
-def get_contacts(self):
-    contacts = get_remote_contacts()
-    for contact in contacts:
-        Contact.objects.update_or_create(
-            name=contact['name'], phone_number=contact['phone_number'], address=contact['address'])
-    return Contact.objects.all()
+    def update_db(self, contacts):
+        for contact in contacts:
+            Contact.objects.update_or_create(
+                name=contact['name'], phone_number=contact['phone_number'], address=contact['address'])
+
+    def search_contacts(self, request, filters):
+        result = Contact.objects.get_queryset()
+        for filter in filters:
+            result = self.filter_by(result, request, filter)
+        return result
+
+    def filter_by(self, collection, request, parameter):
+        var = request.GET.get(parameter)
+
+        if var:
+            return collection.filter(**{parameter + "__icontains": var})
+        return collection
+
+    def get(self, view):
+        contacts = self.get_remote_contacts()
+        self.update_db(contacts)
+        return self.search_contacts(view.request, view.filters)
--- contacts/views.py
+++ contacts/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import ListView
+from django.views.generic.detail import SingleObjectMixin
 
 from .models import Contact
-from . import services
+from .services import ContactsService
 
 # Create your views here.
-class IndexView(generic.ListView):
+class IndexView(ListView):
+  paginate_by = 20
   template_name = 'index.html'
   context_object_name = 'contacts_list'
-
+  service = ContactsService()
+  filters = ("name", "phone_number", "address")
+  
   def get_queryset(self):
-      return services.get_contacts(self)
+      return self.service.get(self)
 
   def get_context_data(self, **kwargs):
       context = super().get_context_data(**kwargs)
       context["title"] = "Contacts"
+      context["search_name"] = self.request.GET.get(self.filters[0])
+      context["search_phone_number"] = self.request.GET.get(self.filters[1])
+      context["search_address"] = self.request.GET.get(self.filters[2])
       return context
   
